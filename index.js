a5f56071dcae08a05a00652e31a4ae6b425cca5d
David Rana Magar <36442527+DavidRanaMagar@users.noreply.github.com>
2024-10-18 00:04:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,3 +44,9 @@ db.sequelize.sync().then(() => {
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
